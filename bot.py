1bcc79968c552795e066d1ab050c0cc367d45002
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-04-22 23:32:12 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-
-##bot.run('OTg3MTQ4ODMxOTk5Mjk1NDg4.GF9qCx._yoIk8w7XsCTd4Nn5z-ShCe8ylxc-qUc3QkLko')
 
 async def main():
     await bot.load_extension("event")
