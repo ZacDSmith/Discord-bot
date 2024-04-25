8e5902e1caa681591582eec2e0950b72613d9691
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-04-24 20:11:44 -0700

    CHATBOT WORKS
    did some research and realized I was a big dummy head and then I fixed it!

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,10 @@ from discord.ext import commands
 import aiohttp
 import os
 from dotenv import load_dotenv
-
+"""
+Add a command that lets you input a prompt and it generates a picture
+Add a commamd that uses image recognition for screenshots.
+"""
 load_dotenv()
 apikey=os.environ['GPT']
 
@@ -18,19 +21,18 @@ class Chatbot(commands.Cog):
         async with aiohttp.ClientSession() as session:
             payload= {
                 "model": "gpt-3.5-turbo",
-                "prompt": prompt,
+                "messages": [{"role": "user", "content": prompt}],
                 "temperature": 0.5,
                 "max_tokens": 50,
                 "presence_penalty": 0,
-                "frequency_penalty": 0,
-                "best_of": 1,
+                "frequency_penalty": 0
             }
             headers = {"Authorization": f"Bearer {apikey}"}
-            async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
+            async with session.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers) as resp:
                 response = await resp.json()
                 print(response)
-                embed = discord.Embed(title="Chat GPT's Response:", description=response["choices"][0]["text"])
-                await ctx.reply(embed=embed)
+                embed = discord.Embed(title="Chat GPT's Response:", description=response["choices"][0]["message"]["content"])
+                await ctx.send(embed=embed)
 
 
 async def setup(bot):
