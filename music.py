50ed3a2553eb6310e4a118c5ee9be2ccad626d41
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-04-25 21:37:57 -0700

    Added Commands Music.py
    Added !stop
    !pause
    !resume

--- music.py
+++ music.py
@@ -11,42 +11,56 @@ import re
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
 client = discord.Client(intents=discord.Intents.all())
 class Music(commands.Cog):
-
     def __init__(self, bot) -> None:
         self.bot = bot
+        self.voice_client = None
+        self.is_playing = False
 
     @commands.command()
     async def play(self,ctx:commands.Context, url: str):
-        try:
+        try:    
+            voice_channel = ctx.author.voice.channel 
+            if self.is_playing:
+                vc = self.voice_client
+            else:
+                vc = await voice_channel.connect()
+            self.is_playing = True
+            self.voice_client = vc
             html = urllib.request.urlopen(url)
             video_ids = re.findall(r"watch\?v=(\S{11})",html.read().decode())
-
             await ctx.message.delete()
             await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
             yt = YouTube(url)
 
             stream = yt.streams.filter(only_audio=True).first()
-            voice_channel = ctx.author.voice.channel 
-            vc = await voice_channel.connect()
             vc.play(discord.FFmpegPCMAudio(executable="C:/Users/Ratfi/OneDrive/Desktop/ffmpeg-2024-04-21-git-20206e14d7-full_build/bin/ffmpeg.exe", source=f"{stream.url}", **FFMPEG_OPTIONS))
             await ctx.send('Now playing...')
         except Exception as e:
             print(e)
-"""
-PLEASE GET WORKING
+
+
+    @commands.command()
+    async def stop(self,ctx: commands.Context):
+        try:
+            await ctx.voice_client.disconnect()
+            await ctx.send('Stopped...')
+            self.is_playing = False
+        except Exception as e:
+             print(e)    
+
     @commands.command()
     async def pause(self,ctx: commands.Context):
-        voice_channel = ctx.author.voice.channel 
-        vc = await voice_channel.connect()
-        await vc.pause(discord.FFmpegPCMAudio(executable="C:/Users/Ratfi/OneDrive/Desktop/ffmpeg-2024-04-21-git-20206e14d7-full_build/bin/ffmpeg.exe"))
-        await ctx.send("paused")
+        self.voice_client.pause()
+        await ctx.message.delete()
+        await ctx.send('Paused...')
+        self.is_playing = False
 
     @commands.command()
-    async def resume(self,ctx: commands.Context):
-        await vc.resume()
-        await ctx.send("resumed")
-"""
-
+    async def resume(self, ctx):
+        await ctx.message.delete()
+        self.voice_client.resume()
+        await ctx.send('Resumed...')
+        self.is_playing = True
 
 async def setup(bot):
     await bot.add_cog(Music(bot))
