456bf624a668b0506fe8979784f9585053ac9b6b
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-04-27 20:52:21 -0700

    formatting and stuff

--- chatbot.py
+++ chatbot.py
@@ -14,21 +14,23 @@ class Chatbot(commands.Cog):
 
     @commands.command()
     async def chat(self, ctx: commands.Context, *, prompt:str):
-        async with aiohttp.ClientSession() as session:
-            payload= {
-                "model": "gpt-3.5-turbo",
-                "messages": [{"role": "user", "content": prompt}],
-                "temperature": 0.5,
-                "max_tokens": 50,
-                "presence_penalty": 0,
-                "frequency_penalty": 0
-            }
-            headers = {"Authorization": f"Bearer {apikey}"}
-            async with session.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers) as resp:
-                response = await resp.json()
-                embed = discord.Embed(title="Chat GPT's Response:", description=response["choices"][0]["message"]["content"])
-                await ctx.send(embed=embed)
-
+        try:
+            async with aiohttp.ClientSession() as session:
+                payload= {
+                    "model": "gpt-3.5-turbo",
+                    "messages": [{"role": "user", "content": prompt}],
+                    "temperature": 0.5,
+                    "max_tokens": 50,
+                    "presence_penalty": 0,
+                    "frequency_penalty": 0
+                }
+                headers = {"Authorization": f"Bearer {apikey}"}
+                async with session.post("https://api.openai.com/v1/chat/completions", json=payload, headers=headers) as resp:
+                    response = await resp.json()
+                    embed = discord.Embed(title="Chat GPT's Response:", description=response["choices"][0]["message"]["content"])
+                    await ctx.send(embed=embed)
+        except Exception as e:
+            print(e)
 
 async def setup(bot):
     await bot.add_cog(Chatbot(bot))
--- imageai.py
+++ imageai.py
@@ -14,21 +14,25 @@ class ImageGen(commands.Cog):
 
     @commands.command()
     async def image(self,ctx:commands.Context, *, prompt:str):
-        async with aiohttp.ClientSession() as session:
-            payload={
-                "model": "dall-e-3",
-                "prompt": prompt,
-                "size":"1024x1024",
-                "quality":"standard",
-                "n":1,
-            }
-            headers = {"Authorization": f"Bearer {apikey}"}
-            async with session.post("https://api.openai.com/v1/images/generations", json=payload, headers=headers) as resp:
-                response = await resp.json()
-                embed = discord.Embed(description=response["data"][0]["revised_prompt"])
-                embed.set_image(url=response["data"][0]["url"])
-                #await ctx.send(response["data"][0]["url"])
-                await ctx.send(embed=embed)
+        try:
+            async with aiohttp.ClientSession() as session:
+                await ctx.send("Generating Image...")
+                payload={
+                    "model": "dall-e-3",
+                    "prompt": prompt,
+                    "size":"1024x1024",
+                    "quality":"standard",
+                    "n":1,
+                }
+                headers = {"Authorization": f"Bearer {apikey}"}
+                async with session.post("https://api.openai.com/v1/images/generations", json=payload, headers=headers) as resp:
+                    response = await resp.json()
+                    embed = discord.Embed(description=response["data"][0]["revised_prompt"])
+                    embed.set_image(url=response["data"][0]["url"])
+                    #await ctx.send(response["data"][0]["url"])
+                    await ctx.send(embed=embed)
+        except Exception as e:
+            print(e)
                 
 
 async def setup(bot):
