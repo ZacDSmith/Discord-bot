d6adae3acfa741ad1a9885ef3ff42d78699b59b6
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-05-02 23:04:59 -0700

    Creates ticket channel
    Read Readme

--- ticket.py
+++ ticket.py
@@ -11,19 +11,37 @@ ticket system:
 from discord.ext import commands
 import discord
 
+class buttons(discord.ui.View):
+ #GET SOME
+    @discord.ui.button(label="Open Ticket", style=discord.ButtonStyle.blurple)
+    async def ticketbtn(self, interations: discord.Interaction, button: discord.ui.Button):
+        await interations.response.send_message("Ticket channel created")
+        guild = interations.guild
+        user = guild.get_member(interations.user.id)
+        ticket_channel_create = await guild.create_text_channel('Ticket🎫')
+        channel = discord.utils.get(guild.channels, name=f"{ticket_channel_create}")
+        channel_id = channel.id
+        ticket_channel = guild.get_channel(channel_id)
+        await ticket_channel.set_permissions(guild.default_role, send_messages=False, view_channel=False)
+        await ticket_channel.set_permissions(user, send_messages=True, view_channel=True)
+
 class Ticket(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot:commands.Bot = bot
 
+    #
     @commands.command()
-    async def test(self, ctx: commands.Context):
+    async def test(self, ctx: commands.Context, ):
         try:
             guild = ctx.message.guild
-            ticket_channel_create = await guild.create_text_channel('ticket-channel')
+            ticket_channel_create = await guild.create_text_channel('🎫TICKETS🎫')
             channel = discord.utils.get(ctx.guild.channels, name=f"{ticket_channel_create}")
             channel_id = channel.id
             ticket_channel = self.bot.get_channel(channel_id)
-            await ticket_channel.send("TICKET CHANNEL NERDS")
+            await ticket_channel.set_permissions(ctx.guild.default_role, send_messages=False)
+            embed = discord.Embed(title=f"TICKETS", color= discord.Color.green())
+            await ticket_channel.send(embed=embed, view=buttons())
+
         except Exception as e:
             print(e)
 
