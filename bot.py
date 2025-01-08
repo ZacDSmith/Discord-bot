0f7583aa8f920e96988008bcefe1cd1faa1501bc
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-01-08 15:57:51 -0500

    discord channel user time log

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
-from event import Event
 from discord.ext import commands
 import asyncio
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+import json
 
 load_dotenv()
 
@@ -16,6 +17,24 @@ bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
 
 
 async def main():
+    join_times = {}
+
+    @bot.event
+    async def on_voice_state_update(member, before, after):
+        # Check if the user has joined a voice channel
+        if before.channel is None and after.channel is not None:
+            join_times[member.name] = datetime.now()
+
+        # Check if the user has left a voice channel
+        elif before.channel is not None and after.channel is None:
+            if member.name in join_times:
+                join_duration = datetime.now() - join_times[member.name]
+                print(f"User {member.name} was in the voice channel for {join_duration}")
+                total_time = {f"{join_duration}": {"name": member.name}}
+                with open('time.json', 'w') as f:
+                    json.dump(total_time, f, indent=4)
+                del join_times[member.name]
+
     await bot.load_extension("event")
     await bot.load_extension("commands")
     await bot.load_extension("shop")
