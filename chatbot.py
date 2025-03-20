56b4fc799be15cc7569c7b79cbb39d5033eb76b0
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-03-20 00:11:04 -0400

    Fixed music bot

--- chatbot.py
+++ chatbot.py
@@ -3,9 +3,11 @@ from discord.ext import commands
 import aiohttp
 import os
 from dotenv import load_dotenv
+import ollama
 
 load_dotenv()
 apikey = os.environ['GPT']
+clients = ollama.Client()
 
 
 class Chatbot(commands.Cog):
@@ -13,27 +15,43 @@ class Chatbot(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    @commands.command(name="chat", help="Responds to a prompt entered by the user.")
-    async def chat(self, ctx: commands.Context, *, prompt: str):
+    # @commands.command(name="chat", help="Responds to a prompt entered by the user.")
+    # async def chat(self, ctx: commands.Context, *, prompt: str):
+    #     try:
+    #         async with aiohttp.ClientSession() as session:
+    #             payload = {
+    #                 "model": "gpt-3.5-turbo",
+    #                 "messages": [{"role": "user", "content": prompt}],
+    #                 "temperature": 0.5,
+    #                 "max_tokens": 50,
+    #                 "presence_penalty": 0,
+    #                 "frequency_penalty": 0
+    #             }
+    #             headers = {"Authorization": f"Bearer {apikey}"}
+    #             async with session.post("https://api.openai.com/v1/chat/completions", json=payload,
+    #                                     headers=headers) as resp:
+    #                 response = await resp.json()
+    #                 embed = discord.Embed(title="Chat GPT's Response:",
+    #                                       description=response["choices"][0]["message"]["content"])
+    #                 await ctx.send(embed=embed)
+    #     except Exception as e:
+    #         print(e)
+    def split_string_every_2000_chars(input_string):
+        return [input_string[i:i + 2000] for i in range(0, len(input_string), 2000)]
+
+    @commands.command(name="google", description="ai chatbot that can write code.")
+    async def google(self, ctx, prompt: str):
+        model = "deepseek-r1:8b"
         try:
-            async with aiohttp.ClientSession() as session:
-                payload = {
-                    "model": "gpt-3.5-turbo",
-                    "messages": [{"role": "user", "content": prompt}],
-                    "temperature": 0.5,
-                    "max_tokens": 50,
-                    "presence_penalty": 0,
-                    "frequency_penalty": 0
-                }
-                headers = {"Authorization": f"Bearer {apikey}"}
-                async with session.post("https://api.openai.com/v1/chat/completions", json=payload,
-                                        headers=headers) as resp:
-                    response = await resp.json()
-                    embed = discord.Embed(title="Chat GPT's Response:",
-                                          description=response["choices"][0]["message"]["content"])
-                    await ctx.send(embed=embed)
+            response = clients.generate(model=model, prompt=prompt)
+            #input_string = response.response  # replace with your string
+            #chunks = split_string_every_2000_chars(input_string)
+            await ctx.send(f"Prompt: {prompt}. \n Response:")
+            #for chunk in chunks:
+                #await ctx.send(f"{chunk}")
+            await ctx.send(f"Prompt: {prompt}. \n Response: {response.response}")
         except Exception as e:
-            print(e)
+            print("Error occurred: ", e)
 
 
 async def setup(bot):
--- music.py
+++ music.py
@@ -1,14 +1,17 @@
+import asyncio
 import os.path
-import urllib.request
 import discord
 from discord.ext import commands
 from pytubefix import YouTube
-import urllib
-import re
 from discord import File
+import yt_dlp
 
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+voice_clients = {}
+yt_dl_options = {"format": "bestaudio/best"}
+ytdl = yt_dlp.YoutubeDL(yt_dl_options)
 
+#FFMPEG_OPTIONS = {'options': '-vn'}
 
 class Music(commands.Cog):
     def __init__(self, bot) -> None:
@@ -17,29 +20,27 @@ class Music(commands.Cog):
         self.is_playing = False
 
     @commands.command(name="play", help="plays youtube audio by url")
-    async def play(self, ctx: commands.Context, url: str):
+    async def play(self, ctx):
         try:
-            voice_channel = ctx.author.voice.channel
-            if self.is_playing:
-                vc = self.voice_client
-            else:
-                vc = await voice_channel.connect()
-            self.is_playing = True
-            self.voice_client = vc
-            html = urllib.request.urlopen(url)
-            video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-            await ctx.message.delete()
-            await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
-            yt = YouTube(url)
-            stream = yt.streams.filter(only_audio=True).first()
-            vc.pause()
-            ## YOU HAVE TO DOWNLOAD THE ffmpeg.exe AND CHANGE THE FILE PATH TO YOURS.
-            vc.play(discord.FFmpegPCMAudio(
-                executable="C:/Program Files (x86)/ffmpeg-2024-04-21-git-20206e14d7-full_build/bin/ffmpeg.exe",
-                source=f"{stream.url}", **FFMPEG_OPTIONS))
-            await ctx.send('Now playing...')
+            voice_client = await ctx.message.author.voice.channel.connect()
+            voice_clients[voice_client.guild.id] = voice_client
         except Exception as e:
             print(e)
+
+        try:
+            url = ctx.message.content.split()[1]
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+            song = data['url']
+            player = discord.FFmpegPCMAudio(song, **FFMPEG_OPTIONS)
+            voice_clients[ctx.message.guild.id].play(player)
+            await ctx.message.delete()
+            await ctx.send(f"{url} now playing!")
+
+        except Exception as e:
+            print(e)
+
+
 
     @commands.command(name="download", help="Downloads MP3 from youtube")
     async def download(self, ctx: commands.Context, url: str):
@@ -47,7 +48,7 @@ class Music(commands.Cog):
             await ctx.message.delete()
             yt = YouTube(url)
             stream = yt.streams.filter(only_audio=True).first()
-            destination = '.'
+            destination = ''
             out_file = stream.download(output_path=destination)
             base, ext = os.path.splitext(out_file)
             new_file = base + '.mp3'
